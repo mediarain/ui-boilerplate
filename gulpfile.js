b7dc170bedad493b6e0ed35b18800713948198eb
Matt Darrington <matt_darrington@yahoo.com>
2015-04-16 10:49:34 -0600

    added a few of my favorite processes to gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,26 @@
 "use strict";
 
-var gulp                     = require('gulp')
-      , less                    = require('gulp-less')
-      , watch                 = require('gulp-watch')
-      , runSequence     = require('run-sequence')
-      , concat                = require('gulp-concat')
-      , minifyCss           = require('gulp-minify-css')
-      , uglify                   = require('gulp-uglify')
+var gulp                   = require('gulp')
+    , less                 = require('gulp-less')
+    , watch                = require('gulp-watch')
+    , runSequence          = require('run-sequence')
+    , concat               = require('gulp-concat')
+    , minifyCss            = require('gulp-minify-css')
+    , uglify               = require('gulp-uglify')
+    , autoprefixer         = require('gulp-autoprefixer')
+    , rename               = require('gulp-rename')
 
-      , styleBase            = 'styles/'
-      , styleSource        = styleBase + 'less/'
-      , watchLessFiles  = styleSource + '/**/*.less'
-      , lessFiles             = styleSource + '/**/main.less'
+    , styleBase            = 'styles/'
+    , styleSource          = styleBase + 'less/'
+    , cssBuild             = styleBase + 'main.css'
+    , watchLessFiles       = styleSource + '/**/*.less'
+    , lessFiles            = styleSource + '/**/main.less'
 
-      , jsBase                = 'js/'
-      , jsSource             = jsBase + 'lib/'
-      , watchJsFiles       = jsSource + '/**/*.js'
-      ;
+    , jsBase               = 'js/'
+    , jsSource             = jsBase + 'lib/'
+    , jsBuild              = jsBase + 'main.js'
+    , watchJsFiles         = jsSource + '/**/*.js'
+    ;
 
 gulp.task('default', function (cb) {
   runSequence(['less', 'js'], cb);
@@ -25,15 +29,31 @@ gulp.task('default', function (cb) {
 gulp.task('less', function() {
   return gulp.src(lessFiles)
   .pipe(less())
-  // .pipe(minifyCss()) // Use when ready to go to production - minify/uglify
   .pipe(concat('main.css'))
+  .pipe(autoprefixer({
+    browsers: ['last 2 versions'],
+    cascade: false
+  }))
   .pipe(gulp.dest(styleBase))
 });
 
 gulp.task('js', function() {
   return gulp.src(watchJsFiles)
-  // .pipe(uglify()) // Use when ready to go to production - minify/uglify
   .pipe(concat('main.js'))
+  .pipe(gulp.dest(jsBase))
+});
+
+gulp.task('buildCss', function(){
+  return gulp.src(cssBuild)
+  .pipe(minifyCss())
+  .pipe(rename({ suffix: '.min' }))
+  .pipe(gulp.dest(styleBase))
+});
+
+gulp.task('buildJs', function(){
+  return gulp.src(jsBuild)
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest(jsBase))
 });
 
@@ -45,3 +65,7 @@ gulp.watch(watchJsFiles, function(cb) {
   runSequence('js');
 });
 
+/* Use when going to production to minify css and js */
+gulp.task('build', function(){
+  runSequence('buildCss', 'buildJs');
+});
