bf79ce9201796fe1e5a9d8ee904e2d3280a3b2b8
Tyler Nielsen <tynielsen@gmail.com>
2015-09-23 15:13:56 -0600

    making boilerplate a little more generic

--- default/gulpfile.js
+++ default/gulpfile.js
@@ -1,25 +1,25 @@
 "use strict";
 
-var gulp                   = require('gulp')
-    , less                 = require('gulp-less')
-    , watch                = require('gulp-watch')
-    , runSequence          = require('run-sequence')
-    , concat               = require('gulp-concat')
-    , minifyCss            = require('gulp-minify-css')
-    , uglify               = require('gulp-uglify')
-    , autoprefixer         = require('gulp-autoprefixer')
-    , rename               = require('gulp-rename')
+var gulp = require('gulp')
+    , less = require('gulp-less')
+    , watch = require('gulp-watch')
+    , runSequence = require('run-sequence')
+    , concat = require('gulp-concat')
+    , minifyCss = require('gulp-minify-css')
+    , uglify = require('gulp-uglify')
+    , autoprefixer = require('gulp-autoprefixer')
+    , rename = require('gulp-rename')
 
-    , styleBase            = 'styles/'
-    , styleSource          = styleBase + 'less/'
-    , cssBuild             = styleBase + 'main.css'
-    , watchLessFiles       = styleSource + '/**/*.less'
-    , lessFiles            = styleSource + '/**/main.less'
+    , styleBase = 'styles/'
+    , styleSource = styleBase + 'less/'
+    , cssBuild = styleBase + 'main.css'
+    , watchLessFiles = styleSource + '/**/*.less'
+    , lessFiles = styleSource + '/**/main.less'
 
-    , jsBase               = 'js/'
-    , jsSource             = jsBase + 'lib/'
-    , jsBuild              = jsBase + 'main.js'
-    , watchJsFiles         = jsSource + '/**/*.js'
+    , jsBase = 'js/'
+    , jsSource = jsBase + 'lib/'
+    , jsBuild = jsBase + 'main.js'
+    , watchJsFiles = jsSource + '/**/*.js'
     ;
 
 gulp.task('less', function() {
@@ -31,7 +31,7 @@ gulp.task('less', function() {
   })
   .pipe(concat('main.css'))
   .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
+    browsers: ['last 4 versions'],
     cascade: false
   }))
   .pipe(gulp.dest(styleBase))
@@ -39,27 +39,21 @@ gulp.task('less', function() {
 
 gulp.task('js', function() {
   return gulp.src(watchJsFiles)
-  .pipe(concat('main.js'))
+  .pipe(concat('libs.js'))
+  .pipe(uglify())
+  .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest(jsBase))
 });
 
-gulp.task('buildCss', function(){
+gulp.task('minCss', function(){
   return gulp.src(cssBuild)
   .pipe(minifyCss())
   .pipe(rename({ suffix: '.min' }))
   .pipe(gulp.dest(styleBase))
 });
 
-gulp.task('buildJs', function(){
-  return gulp.src(jsBuild)
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest(jsBase))
-});
-
 gulp.task('default', function (cb) {
   gulp.watch(watchLessFiles, ['less']);
   gulp.watch(watchJsFiles, ['js']);
-  runSequence(['less', 'js'], /* ['buildCss','buildJs'], */ cb);
+  runSequence(['less', 'js'], /* ['minCss'], */ cb);
 });
-
