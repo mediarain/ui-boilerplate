51cd413a0ecbdfc78d636a099ee8687c14c3ad55
Tyler Nielsen <tynielsen@gmail.com>
2016-03-22 13:41:21 -0600

    updating wordpress boilerplate to match wp standards

--- wordpress/gulpfile.js
+++ wordpress/gulpfile.js
@@ -1,65 +1,88 @@
+/**
+ * Gulp settings.
+ *
+ * @package cktheme
+ */
+
 "use strict";
 
-var gulp                   = require('gulp')
-    , less                 = require('gulp-less')
-    , watch                = require('gulp-watch')
-    , runSequence          = require('run-sequence')
-    , concat               = require('gulp-concat')
-    , minifyCss            = require('gulp-minify-css')
-    , uglify               = require('gulp-uglify')
-    , autoprefixer         = require('gulp-autoprefixer')
-    , rename               = require('gulp-rename')
+var gulp					= require( 'gulp' )
+	, less					= require( 'gulp-less' )
+	, indent				= require( './custom-gulp/indent' )
+	, watch					= require( 'gulp-watch' )
+	, runSequence			= require( 'run-sequence' )
+	, concat				= require( 'gulp-concat' )
+	, minifyCss				= require( 'gulp-minify-css' )
+	, uglify				= require( 'gulp-uglify' )
+	, autoprefixer			= require( 'gulp-autoprefixer' )
+	, rename				= require( 'gulp-rename' )
 
-    , styleBase            = './'
-    , styleSource          = styleBase + 'less/'
-    , cssBuild             = styleBase + 'style.css'
-    , watchLessFiles       = styleSource + '/**/*.less'
-    , lessFiles            = styleSource + '/**/main.less'
+	, styleBase				= './'
+	, styleSource			= styleBase + 'less/'
+	, cssBuild				= styleBase + 'style.css'
+	, watchLessFiles		= styleSource + '/**/*.less'
+	, lessFiles				= styleSource + '/**/main.less'
 
-    , jsBase               = 'js/'
-    , jsSource             = jsBase + 'lib/'
-    , jsBuild              = jsBase + 'main.js'
-    , watchJsFiles         = jsSource + '/**/*.js'
-    ;
+	, jsBase				= 'scripts/'
+	, jsLib					= jsBase + 'lib/'
+	, jsSlides				= jsBase + 'slides/**/*.js'
+	, jsTemplates			= jsBase + 'templates/**/*.js'
+	, jsMain				= jsBase + 'main.js'
+	, watchJsLibFiles		= jsLib + '/**/*.js'
+	, phpcs					= require( 'gulp-phpcs' );
 
-gulp.task('less', function() {
-  return gulp.src(lessFiles)
-  .pipe(less())
-  .on('error', function(err) {
-    console.log(err);
-    this.emit('end');
-  })
-  .pipe(concat('style.css'))
-  .pipe(autoprefixer({
-    browsers: ['last 2 versions'],
-    cascade: false
-  }))
-  .pipe(gulp.dest(styleBase))
+gulp.task( 'less', function() {
+	return gulp.src( lessFiles )
+	.pipe( less() )
+	.on( 'error', function( err ) {
+		console.log( err );
+		this.emit( 'end' );
+	})
+	.pipe( concat( 'style.css' ) )
+	.pipe( autoprefixer({
+		browsers: ['last 4 versions'],
+		cascade: false
+	}))
+	.pipe(indent({
+		tabs: true,
+		amount: 1
+	}))
+	.pipe( gulp.dest( styleBase ) )
+	// .pipe( minifyCss( {compatibility: 'ie9'} ) )
 });
 
-gulp.task('minifyCss', function(){
-  return gulp.src(cssBuild)
-  .pipe(minifyCss())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest(styleBase))
+gulp.task( 'js_libs', function() {
+	return gulp.src( watchJsLibFiles )
+	.pipe( concat( 'libs.js' ) )
+	.pipe( uglify() )
+	.pipe( rename( { suffix: '.min' } ) )
+	.pipe( gulp.dest( jsBase ) )
 });
 
-gulp.task('js', function() {
-  return gulp.src(watchJsFiles)
-  .pipe(concat('main.js'))
-  .pipe(gulp.dest(jsBase))
+gulp.task( 'min_js_main', function() {
+	return gulp.src( [jsMain, jsTemplates, jsSlides] )
+	.pipe( concat( 'main.min.js' ) )
+	.pipe( uglify() )
+	.pipe( gulp.dest( jsBase ) )
 });
 
-gulp.task('minifyJs', function(){
-  return gulp.src(jsBuild)
-  .pipe(uglify())
-  .pipe(rename({ suffix: '.min' }))
-  .pipe(gulp.dest(jsBase))
+gulp.task('phpcs', function () {
+	return gulp.src( ['./**/*.php', '!src/vendor/**/*.*'] )
+		// Validate files using PHP Code Sniffer.
+		.pipe(phpcs({
+			bin: 'c:\\php\\phpcs',
+			standard: 'WordPress-Strict-Windows',
+			warningSeverity: 0
+		}))
+		// Log all problems that was found.
+		.pipe( phpcs.reporter( 'log' ) );
 });
 
-gulp.task('default', function (cb) {
-  gulp.watch(watchLessFiles, ['less']);
-  gulp.watch(watchJsFiles, ['js']);
-  runSequence(['less', 'js'], cb);
-});
+gulp.task( 'default', function (cb) {
+	gulp.watch( watchLessFiles, ['less'] );
+	gulp.watch( watchLessFiles, ['liveChatLess'] );
+	gulp.watch( watchJsLibFiles, [ 'js_libs' ] );
+	gulp.watch( [jsSlides, jsTemplates, jsMain], [ 'min_js_main' ] );
 
+	runSequence( [ 'less', 'liveChatLess', 'js_libs', 'min_js_main' ], cb );
+});
--- wordpress/header.php
+++ wordpress/header.php
@@ -10,15 +10,15 @@
 <html <?php language_attributes(); ?>>
 <head>
 
-  <meta charset="<?php bloginfo( 'charset' ); ?>">
-  <meta http-equiv="Content-Type" content="text/html; <?php bloginfo( 'charset' ); ?>" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="<?php bloginfo( 'charset' ); ?>">
+	<meta http-equiv="Content-Type" content="text/html; <?php bloginfo( 'charset' ); ?>" />
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <link rel="profile" href="http://gmpg.org/xfn/11">
-  <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
-  <?php wp_head(); ?>
+	<?php wp_head(); ?>
 
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/img/apple-touch-icon.png">
 	<link type="img/png" rel="icon" href="<?php echo get_template_directory_uri(); ?>/img/favicon.png">
@@ -26,38 +26,37 @@
 </head>
 <body <?php body_class(); ?>>
 <!--[if lt IE 8]>
-  <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
+	<p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
 
-  <div class="wrapper">
+	<div class="wrapper">
 
+		<header>
 
-    <header>
+			<div class="row-one row">
+				<div class="container">
 
-      <div class="row-one row">
-        <div class="container">
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-		  <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php wp_nav_menu( array( 'theme_location' => 'secundary', 'menu_class' => 'toolbar' ) ); ?>
 
-		  <?php wp_nav_menu( array( 'theme_location' => 'secundary', 'menu_class' => 'toolbar' ) ); ?>
+				</div> <!-- /.container -->
+			</div> <!-- /.row-one -->
 
-        </div>
-      </div> <!-- /.row-one -->
+			<div class="row-two row">
+				<div class="container">
 
-      <div class="row-two row">
-        <div class="container">
+					<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
+						<img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="">
+					</a>
 
-          <a class="logo" href="<?php echo esc_url( home_url( '/' ) ); ?>">
-            <img src="<?php echo get_template_directory_uri(); ?>/img/logo.png" alt="">
-          </a>
+					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav' ) ); ?>
 
-          <?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav' ) ); ?>
+					<div class="search-box">
+						<?php get_search_form(); ?>
+					</div> <!-- /.search-box -->
 
-			<div class="search-box">
-				<?php get_search_form(); ?>
-			</div>
+				</div> <!-- /.container -->
+			</div> <!-- /.row-two -->
 
-        </div>
-      </div> <!-- /.row-two -->
-
-    </header>
+		</header>
